4a2fe1697b5488fc3f97d9a59efd92121f1add23
maknetaRo <makneta@gmail.com>
2023-03-22 19:55:46 +0100

    is any of 3 in range

--- isInRange.js
+++ isInRange.js
@@ -11,3 +11,12 @@ const areInRange = (a, b) => {
 console.log(areInRange(3, 49));
 console.log(areInRange(33, 89));
 console.log(areInRange(50, 99));
+
+const ofThree = (a, b, c) => {
+  return isTrue(a) || isTrue(b) || isTrue(c) ? true : false;
+};
+
+console.log(areInRange(3, 49, -56));
+console.log(areInRange(33, 89, 22));
+console.log(areInRange(50, 99, 12));
+console.log(areInRange(50, 99, 72));
