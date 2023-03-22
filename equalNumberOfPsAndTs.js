b3accd6c83f3d92baa1c9ceb3e861faa8690381c
maknetaRo <makneta@gmail.com>
2023-03-22 21:49:25 +0100

    if number of p's and t's is equal in a given string

---
+++ equalNumberOfPsAndTs.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program to check whether a given string contains equal number of p's and t's.
+
+const isEqual = (str) => {
+  return (str.match(/p/g) || []).length == (str.match(/t/g) || []).length
+    ? true
+    : false;
+};
+
+console.log(isEqual("proton"));
+console.log(isEqual("parapet"));
+
+const isEqual2 = (str) => {
+  return str.split("p").length == str.split("t").length ? true : false;
+};
+console.log(isEqual("proton"));
+console.log(isEqual("parapet"));
